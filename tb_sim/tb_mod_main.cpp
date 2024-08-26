ae655065ff383c14f04e0cc089be2120cc286fd4
VicoHBB <vhbb98@gmail.com>
2024-08-25 23:26:46 -0600

    Draft of the Template
    The configuration of the tools and recipes has been tested, but the
    documentation still needs improvement.

---
+++ tb_sim/tb_mod_main.cpp
@@ -0,0 +1,131 @@
+#include <cstdint>
+#include "/usr/share/verilator/include/verilated.h"   /* Put whole route so that it does not mark errors */
+//#include <verilated.h>                              [> but you can call the file like this <]
+#include "/usr/share/verilator/include/verilated_vcd_c.h"
+//#include <verilated_vcd_c.h>
+#include <iostream>                                   /* Need std::cout */
+#include "../obj_dir/Vmod_main.h"                     /* From Verilating "mod_main.sv" */
+#include "tb_mod_main.hpp"                            /* CAll some definitions */
+
+#define MAX_SIM_TIME 2000            /* Duration of the simulation in ps */
+#define NPs          20              /* Pulse width in ps */
+
+double sc_time_stamp( void );                  /* Called by $time in verilog */
+void Param_Init( void );                       /* Parameter initialization */
+void Set_In_D( uint8_t tag, uint8_t value );   /* Set Input D */
+
+Vmod_main *top;                         /* Instantiation of the mode */
+
+// The 64-bit integer reduce wrap-over issues and allow modulus.
+// This is in units of the time precision
+// used in Verilog (or from --timescale-override)
+uint64_t clock_signal = 0;             /* Current simulation time */
+// The 64-bit integer reduce qrap over issues and allow modulus.
+// This is in units of the time precision
+// used in Verilog (or from --timescale-override)
+
+int counter;
+
+int main( int argc, char *argv[] )
+{
+    Verilated::commandArgs( argc, argv );       // Remember args
+    Verilated::traceEverOn(true);
+
+    top = new Vmod_main;                            // Create model
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    // Do not instead make Vmain as a files-scope static
+    // variable, as the "C== static initialization order fiasco"
+    // may cause a crash
+
+    top->trace(m_trace, 5);
+    m_trace->open("tb_sim/waveform.vcd");
+
+    int counter = 0;
+    int cycles   = 0;
+
+    while ( clock_signal < MAX_SIM_TIME )
+    {
+
+        switch ( cycles )
+        {
+            case 0:
+                Param_Init();
+                break;
+            case 1:
+                top->i_E = 1u;
+                Set_In_D( 0x0u, 0x01u );
+                break;
+            case 2:
+                top->i_E = 1u;
+                Set_In_D( 0x1u, 0x0Au );
+                break;
+            case 3:
+                top->i_E = 1u;
+                Set_In_D( 0x02u, 0x0B );
+                break;
+            case 4:
+                top->i_E = 1u;
+                Set_In_D( 0x03u, 0x0C );
+                break;
+            case 5:
+                top->i_E = 0u;
+                Set_In_D( 0x04u, 0x0D );
+                break;
+            case 6:
+                top->i_E = 0u;
+                Set_In_D( 0x05u, 0x0E );
+                break;
+            case 7:
+                top->i_E = 0u;
+                Set_In_D( 0x06u, 0x0F );
+                break;
+            case 8:
+                top->i_E = 1u;
+                Set_In_D( 0x07u, 0x10 );
+                break;
+            default:
+                top->i_E = 1u;
+                Set_In_D( 0u, 0u );
+                break;
+
+        }
+
+
+        top->eval();                              // Evaluate mode
+
+        std::cout << top->o_Q << std::endl;       // Read a output
+
+        m_trace->dump( clock_signal );
+
+        top->i_clk = !top->i_clk;                 // Enable or clk signal
+        clock_signal += NPs;                      //Time passes..
+        counter++;
+        cycles = counter / 2u;
+
+    }
+
+    m_trace->close();
+    top->final();                             //Done simulating
+
+    delete top;
+
+    return 0;
+}
+
+double sc_time_stamp( void )    /* Called by $time in verilog */
+{
+    return clock_signal;        // Convets to double, to match what SystemC does
+}
+
+void Set_In_D( uint8_t tag, uint8_t value )
+{
+    top->i_D = ( tag << 8u ) | ( value & 0xFFu );
+}
+
+void Param_Init( void )
+{
+    top->i_clk = 0u;
+    top->i_E = 1u;
+    Set_In_D( 0u, 0u );
+}
+
---
+++ tb_sim/tb_mod_main.hpp
@@ -0,0 +1,5 @@
+#ifndef TB_MOD_MAIN_HPP
+#define TB_MOD_MAIN_HPP
+
+
+#endif
